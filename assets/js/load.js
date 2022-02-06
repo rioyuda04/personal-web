f32e465829c71ba18a66a5036da9b0e9f8bee073
rioyuda04 <rio.yudayanto@outlook.com>
2022-02-06 14:41:06 +0700

    update New

--- assets/js/load.js
+++ assets/js/load.js
@@ -5,7 +5,7 @@ const load = document.querySelector(".navbar")
 // search loaction and get file navbar.html
 //  and then create paramaeter input for initialize format text (string)
 // and then create paramater data for initialize to html  
-fetch("/chapter1/day5-task/navbar.html")
+fetch("navbar.html")
 .then(input=>input.text())
 .then(data=>{
     load.innerHTML=data
